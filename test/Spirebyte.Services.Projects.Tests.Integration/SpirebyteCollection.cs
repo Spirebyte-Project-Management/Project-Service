2231ce71dc691ed5742a37f0261afc4b6bbc9543
Jordi Huntjens <442216@student.fontys.nl>
2020-12-02 22:13:23 +0100

    Disable parallel test execution

--- test/Spirebyte.Services.Projects.Tests.Integration/SpirebyteCollection.cs
+++ test/Spirebyte.Services.Projects.Tests.Integration/SpirebyteCollection.cs
@@ -7,7 +7,7 @@ using Xunit;
 
 namespace Spirebyte.Services.Projects.Tests.Integration
 {
-    [CollectionDefinition("Spirebyte collection")]
+    [CollectionDefinition("Spirebyte collection", DisableParallelization = true)]
     public class SpirebyteCollection : ICollectionFixture<SpirebyteApplicationFactory<Program>>
     {
     }
