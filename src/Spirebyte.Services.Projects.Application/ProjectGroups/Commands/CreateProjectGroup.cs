fc69e72bac5494c96c75cf4dcc1b0543c804648d
Jordi Huntjens <442216@student.fontys.nl>
2022-05-28 02:13:29 +0200

    Update project group creation model

--- src/Spirebyte.Services.Projects.Application/ProjectGroups/Commands/CreateProjectGroup.cs
+++ src/Spirebyte.Services.Projects.Application/ProjectGroups/Commands/CreateProjectGroup.cs
@@ -6,7 +6,7 @@ namespace Spirebyte.Services.Projects.Application.ProjectGroups.Commands;
 
 [Contract]
 public record CreateProjectGroup
-    (Guid ProjectGroupId, string ProjectId, string Name, IEnumerable<Guid> UserIds) : ICommand
+    (string ProjectId, string Name, IEnumerable<Guid> UserIds) : ICommand
 {
     public Guid ProjectGroupId = Guid.NewGuid();
 }
